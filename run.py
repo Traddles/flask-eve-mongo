2d9898fa06a43734613dd22d59017eedfbb0051a
Patrik Hillbo <traddles@gmail.com>
2014-07-18 10:09:22 +0200

    Adding by template

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from eve import Eve
-from flask import Flask, url_for, render_template, send_from_directory
+from flask import Flask, url_for, session, g, redirect, request, flash, render_template, send_from_directory
 from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask.ext.mongoengine.wtf import model_form
 from bson.objectid import ObjectId
@@ -10,9 +10,11 @@ import datetime
 app = Eve(settings='conf/settings.py')
 # Flask
 flask = Flask(__name__)
-flask.config.update(
+flask.config.update(dict(
 	DEBUG = True,
-)
+    USERNAME='admin',
+    PASSWORD='default'
+))
 flask.config['DEBUG_TB_PANELS'] = (
 	'flask.ext.debugtoolbar.panels.versions.VersionDebugPanel',
 	'flask.ext.debugtoolbar.panels.timer.TimerDebugPanel',
@@ -39,18 +41,13 @@ class People(engine.DynamicDocument):
 	def __unicode__(self):
 		return self.name
 
-	# meta = {
-		# 'indexes' : ['-created_at'],
-	# 	'ordering' : ['-created_at']
-	# }
-
-def view_people(page=1):
-    paginated_todos = People.objects.paginate(page=page, per_page=10)
-
-@flask.route("/add")
-def add_one():
-	People(lastname="addie").save()
-	return "Saved one 'addie'"
+@flask.route('/add', methods=['POST'])
+def add_entry():
+    # if not session.get('logged_in'):
+    #     abort(401)
+    People(lastname=request.form['lastname'], firstname=request.form['firstname']).save()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
 
 @flask.route("/look")
 def search():
@@ -64,9 +61,14 @@ def api_articles():
 		r = r + " - " + art.lastname
 	return 'List of ' + url_for('api_articles') + ":<br>\n" + r
 
+# @flask.route('/')
+# def index():
+# 	return "<h1>Salam</h1>"
+
 @flask.route('/')
-def index():
-	return "<h1>Salam</h1>"
+def show_entries():
+    entries = People.objects
+    return render_template('show_entries.html', entries=entries)
 
 if __name__ == '__main__':
 	# Flask
